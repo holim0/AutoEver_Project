cf22b436edc124797f5c6940dfeed3b671faf2de
holim0 <holim1226@gmail.com>
2025-02-09 21:34:33 +0900

    chore: tailwind 변수 설정 및 global.css 내 미디어 쿼리 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,33 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        red: '#ff0900',
+        blue: '#4296e4',
+        mint: {
+          700: '#8ddfcc',
+          900: '#70d7bf',
+        },
+        midnight: {
+          100: '#cdd0d2',
+          900: '#05141f',
+        },
+        gray: {
+          10: '#f8f8f8',
+          50: '#f9fafb',
+          100: '#e6e8e9',
+          200: '#cdd0d2',
+          300: '#b4b9bc',
+          400: '#82898f',
+          500: '#697278',
+          700: '#37434c',
+          800: '#333d4b',
+          900: '#191f28',
+        },
+      },
+      lineHeight: {
+        sm: '1.4',
+        md: '1.6',
+        lg: '1.8',
       },
       fontFamily: {
         kia: ['var(--font-kia)', 'Kia Signature Fix', 'Arial', 'Helvetica', 'sans-serif'],
