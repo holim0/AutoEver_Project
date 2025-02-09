e9d2dd29189f4a6561b8cf7f8d2e618c060a4c7a
holim0 <holim1226@gmail.com>
2025-02-09 23:40:20 +0900

    feat: 페이지 메인 타이틀 구성 (#5)

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -4,6 +4,16 @@ const FAQPage = async () => {
   return (
     <main>
       <Header />
+      <div className="min-h-[calc(100vh-var(--header-height)-var(--footer-height))] overflow-hidden px-[var(--side-padding)] pb-[var(--bottom-padding)]">
+        <div className="mx-auto max-w-[1240px]">
+          <h1 className="flex flex-col items-center justify-center font-bold text-[length:var(--h1-fsize)] h-[var(--h1-height)] leading-[var(--line-height-sm)]">
+            자주 묻는 질문{' '}
+            <em className="font-normal text-[length:var(--h1-fsize-sm)] leading-[var(--line-height-md)] mt-[0.4em] text-center break-keep not-italic">
+              궁금하신 내용을 빠르게 찾아보세요.
+            </em>
+          </h1>
+        </div>
+      </div>
     </main>
   );
 };
