6ccecc6f992042162b9bc12d4bce3653c236f38b
holim0 <holim1226@gmail.com>
2025-02-09 14:58:09 +0900

    chore: 필요 아이콘, 이미지 및 font 설정 (#3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+
 import MSWWrapper from '@/components/MSWWrapper';
 import QueryProvider from '@/components/QueryProvider';
-
 import { initMsw } from '@/mocks';
 
 initMsw();
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const kiaFont = localFont({
+  src: [
+    {
+      path: '../fonts/KiaSignatureFixRegular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/KiaSignatureFixBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-kia', // Tailwind에서 사용하기 위한 CSS 변수 설정
 });
 
 export const metadata: Metadata = {
@@ -30,7 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${kiaFont.variable} font-kia`}>
         <MSWWrapper>
           <QueryProvider>{children}</QueryProvider>
         </MSWWrapper>
