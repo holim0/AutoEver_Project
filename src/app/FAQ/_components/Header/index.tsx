6d31d43eea9ad16ecb8fb59e521359d85fd971ba
holim0 <holim1226@gmail.com>
2025-02-09 23:21:26 +0900

    feat: 페이지 헤더 구현 - 반응형은 미적용 (#5)

---
+++ src/app/FAQ/_components/Header/index.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { cn } from '@/utils/cn';
+import WibleBizLogo from '@/assets/logo_wible_lg.svg';
+import { useDetectScroll } from './useDetectScroll';
+
+const NAV_LIST = [
+  { id: 'guide', name: '서비스 소개', href: 'https://wiblebiz.kia.com/Guide', isCurPage: false },
+  { id: 'faq', name: '자주 묻는 질문', href: '/FAQ', isCurPage: true },
+  { id: 'news', name: '새소식', href: 'https://wiblebiz.kia.com/News', isCurPage: false },
+  { id: 'counsel', name: '상담문의', href: 'https://wiblebiz.kia.com/Counsel', isCurPage: false },
+] as const;
+
+const Header = () => {
+  const isPageScrolled = useDetectScroll();
+
+  return (
+    <header
+      className={cn(
+        'sticky top-0 z-[100] px-[var(--side-padding)] transition-shadow',
+        isPageScrolled && 'shadow-sm'
+      )}
+    >
+      <div className="flex relative max-w-[var(--max-width)] h-[var(--header-height)]">
+        <Link className="flex items-center" href="https://wiblebiz.kia.com">
+          <Image
+            className="w-[120px] h-[40px] lg:w-[160px] lg:h-full"
+            src={WibleBizLogo}
+            alt="WibleBIZ Logo"
+            priority
+          />
+        </Link>
+        <nav className="-mr-[20px] flex-1 h-full">
+          <ul className="flex justify-end h-full">
+            {NAV_LIST.map((item) => (
+              <li className="mx-[16px] my-0" key={item.id}>
+                <Link
+                  className={cn(
+                    'px-1 text-[18px] font-semibold lg:leading-[var(--header-height)] relative h-full flex items-center',
+                    item.isCurPage &&
+                      'before:absolute before:bottom-0 before:left-0 before:h-1 before:w-full before:bg-mint-900 before:content-[""]'
+                  )}
+                  href={item.href}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -1,11 +1,10 @@
+import Header from './_components/Header';
+
 const FAQPage = async () => {
-  const data = await fetch('http://localhost:3000/user');
-  const res = await data.json();
-  console.log(res);
   return (
-    <>
-      <h1>FAQPAGE</h1>
-    </>
+    <main>
+      <Header />
+    </main>
   );
 };
 
