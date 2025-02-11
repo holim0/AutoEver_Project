a81f9ebe18a2d35d921dde892aeba59967b48e9b
holim0 <holim1226@gmail.com>
2025-02-11 15:54:17 +0900

    chore: 헤더 영역 스타일 수정 (#5)

--- src/app/FAQ/_components/Header/index.tsx
+++ src/app/FAQ/_components/Header/index.tsx
@@ -19,11 +19,11 @@ const Header = () => {
   return (
     <header
       className={cn(
-        'sticky top-0 z-[100] px-[var(--side-padding)] transition-shadow',
+        ' bg-white sticky top-0 z-[100] px-[var(--side-padding)] transition-shadow mx-auto my-0',
         isPageScrolled && 'shadow-sm'
       )}
     >
-      <div className="flex relative max-w-[var(--max-width)] h-[var(--header-height)]">
+      <div className="flex justify-between relative max-w-[var(--max-width)] h-[var(--header-height)] mx-auto">
         <Link className="flex items-center" href="https://wiblebiz.kia.com">
           <Image
             className="w-[120px] h-[40px] lg:w-[160px] lg:h-full"
@@ -32,15 +32,17 @@ const Header = () => {
             priority
           />
         </Link>
-        <nav className="-mr-[20px] flex-1 h-full">
+        <nav className={cn('xl:-mr-[20px] xl:flex-1 xl:h-full lg:-mr-[20px] lg:flex-1 lg:h-full')}>
           <ul className="flex justify-end h-full">
             {NAV_LIST.map((item) => (
               <li className="mx-[16px] my-0" key={item.id}>
                 <Link
                   className={cn(
                     'px-1 text-[18px] font-semibold lg:leading-[var(--header-height)] relative h-full flex items-center',
-                    item.isCurPage &&
-                      'before:absolute before:bottom-0 before:left-0 before:h-1 before:w-full before:bg-mint-900 before:content-[""]'
+                    'before:absolute before:bottom-0 before:left-0 before:h-1 before:w-0 before:bg-mint-900 before:content-[""]',
+                    'before:transition-[width] before:duration-[0.4s] before:[transition-timing-function:var(--cubic-bezier-primary)]',
+                    'hover:before:w-full hover:before:opacity-50',
+                    item.isCurPage && 'before:w-full'
                   )}
                   href={item.href}
                 >
