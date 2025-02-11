45b746cdf45556ff6c7746d8cc41f7106a70b679
holim0 <holim1226@gmail.com>
2025-02-11 17:47:01 +0900

    chore: FAQ 리스트 스타일 수정 및 반응형 적용 (#5)

--- src/app/FAQ/_components/FAQList/index.tsx
+++ src/app/FAQ/_components/FAQList/index.tsx
@@ -45,19 +45,31 @@ const Items = ({
   return (
     <li className="border-b border-gray-100 text-[var(--faq-list-a-size)]">
       <button
-        className={`relative w-full flex items-center py-[var(--faq-list-a-padding-v)] pr-[calc(var(--px-xlg)+1.6em)] text-left text-lg font-semibold transition-colors duration-300 ${openId === id ? 'bg-gray-10' : 'bg-white'}`}
+        className={`relative w-full flex sm:flex-col md:flex-col sm:items-start md:items-start items-center py-[var(--faq-list-a-padding-v)] pr-[calc(var(--px-xlg)+1.6em)] text-left text-lg font-semibold transition-colors duration-300 ${openId === id ? 'bg-gray-10' : 'bg-white'}`}
         onClick={() => toggleItem(id)}
       >
-        <span className="text-gray-500 mr-2 px-[var(--faq-list-a-padding-h)] w-[8em] font-normal text-center">
-          {categoryName}
-        </span>
-
-        {curActivetab === 'USAGE' && (
-          <span className="text-gray-500 mr-2 px-[var(--faq-list-a-padding-h)] w-[6em] font-normal text-center">
-            {subCategoryName}
+        <div className="flex sm:flex md:flex sm:items-center md:items-center">
+          <span className="text-gray-500 text-[calc(1em-4px)] mr-2 sm:mr-0 md:mr-0 px-[var(--faq-list-a-padding-h)] sm:pr-0 md:pr-0 w-[8em] sm:w-auto md:w-auto font-normal text-center">
+            {categoryName}
           </span>
-        )}
-        <strong className="flex-1 pl-[var(--faq-list-a-padding-h)]">{question}</strong>
+          {curActivetab === 'USAGE' && (
+            <>
+              <Image
+                className="lg:hidden xl:hidden -rotate-90"
+                src={ArrowIcon}
+                alt="Category Arrow Icon"
+                height={16}
+                priority
+              />
+              <span className="text-gray-500 text-[calc(1em-4px)] mr-2 px-[var(--faq-list-a-padding-h)] sm:pl-0 md:pl-0 w-[8em] sm:w-auto md:w-auto font-normal text-center">
+                {subCategoryName}
+              </span>
+            </>
+          )}
+        </div>
+        <strong className="flex-1 text-[length:var(--faq-list-a-size)] pl-[var(--faq-list-a-padding-h)]">
+          {question}
+        </strong>
         <Image
           className={`absolute right-3 size-[var(--ic-md)] transition-transform duration-300 [transition-timing-function:cubic-bezier(1,0,0.2,1)] ${openId === id ? 'rotate-180' : ''}`}
           src={ArrowIcon}
@@ -69,7 +81,7 @@ const Items = ({
         ref={(el) => {
           faqItemRefs.current[id] = el;
         }}
-        className="transition-[max-height] duration-300 [transition-timing-function:cubic-bezier(1,0,0.2,1)] overflow-hidden"
+        className="transition-[max-height] duration-500 [transition-timing-function:cubic-bezier(1,0,0.2,1)] overflow-hidden"
         style={{ maxHeight: openId === id ? `${faqItemRefs.current[id]?.scrollHeight}px` : '0px' }}
       >
         <div
